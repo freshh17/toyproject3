fffe66559ec0b6f030855c84a93031bd3fd6d9a6
LEE SEONGMIN <blueraptor0@gmail.com>
2023-07-18 21:42:55 +0900

    feat : bootstrap 적용을 위한 레이아웃 페이지 추가

--- module-client/src/main/java/com/example/moduleclient/repository/MemberRepository.java
+++ module-client/src/main/java/com/example/moduleclient/repository/MemberRepository.java
@@ -4,6 +4,6 @@ import com.example.moduleclient.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member,String> {
-    //usernaem 증복 체크
+    //username 증복 체크
     Member findByUsername(String username);
 }
